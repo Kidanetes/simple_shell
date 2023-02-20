efaf88b39dfe18d906a8ee51e9160e9ad7a94672
Abrsh7 <abrshn7@gmail.com>
2023-02-20 12:15:25 -0800

    task2345

---
+++ execmd.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void execmd(char **argv){
+    char *cmd = NULL, *actual_command = NULL;
+
+    if (argv){
+        /* get the command */
+        cmd = argv[0];
+
+        /* generate the path to this command before passing it to execve */
+        actual_command = get_location(cmd);
+
+        /* execute the actual command with execve */
+        if (execve(actual_command, argv, NULL) == -1){
+            perror("Error:");
+        }
+    }
+    
+}
