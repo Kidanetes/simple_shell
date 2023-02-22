04206c4bc6efdb8323036f8ba217b0607d5480aa
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-02-22 23:23:11 +0300

    c file

--- 2-more_functions.c
+++ 2-more_functions.c
@@ -6,7 +6,7 @@
  * @args: command to be put in
  * Return: 0 on success
  */
-int *_error(char *argv,__attribute__((unused)) int number, char *args)
+int *_error(char *argv, __attribute__((unused)) int number, char *args)
 {
 	write(2, argv, _strlen(argv));
 	write(2, ": ", 2);
