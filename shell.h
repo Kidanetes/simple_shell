309ab549be547c2e5da4373780fe32b491d7055f
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-19 14:09:45 +0300

    header file for this project

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _strlen(char *s);
+char **_strtok(char *str);
+int number_of_words(char *str);
+
+#endif
