00a479489e38d5118787bd2e9800200f18258aa4
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-19 14:26:06 +0300

    betty fix

--- strtok.c
+++ strtok.c
@@ -6,68 +6,80 @@
  */
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        for (i = 0; *(s + i) != '\0'; i++)
-        {
-        }
-
-        return (i);
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	return (i);
 }
+/**
+ *_strtok - tokinize the string into words
+ * @str: the string to be tokinized
+ *
+ * Return: array of strings
+ */
 char **_strtok(char *str)
 {
-        int j = 0, i , len = 0;
-        char **array, *token;
+	int j = 0, i , len = 0;
+	char **array, *token;
 
-        if (str == NULL)
-                return (NULL);
-        i = number_of_words(str);
-        if (i == 0)
-                return (NULL);
-        array = malloc(sizeof(char *) * (i + 1));
-        if (array == NULL)
-                return (NULL);
-        token = strtok(str, " \t\n");
-        while (token != NULL)
-        {
-
-                len = _strlen(token);
-                array[j] = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	i = number_of_words(str);
+	if (i == 0)
+		return (NULL);
+	array = malloc(sizeof(char *) * (i + 1));
+	if (array == NULL)
+		return (NULL);
+	token = strtok(str, " \t\n");
+	while (token != NULL)
+	{
+		len = _strlen(token);
+		array[j] = malloc(sizeof(char) * (len + 1));
 		if (array[j] == NULL)
-                {
-                        while (--j)
-                                free(array[j]);
-                        free(array);
-                        return (NULL);
-                }
-                i = 0;
-                while (token[i] != '\0')
-                {
-                        array[j][i] = token[i];
-                        i++;
-                }
-                array[j][i] = '\0';
-                j++;
-                token = strtok(NULL, "  \t\n");
-        }
-        array[j] = NULL;
-        return (array);
+		{
+			while (--j)
+				free(array[j]);
+			free(array);
+			return (NULL);
+		}
+		i = 0;
+		while (token[i] != '\0')
+		{
+			array[j][i] = token[i];
+			i++;
+		}
+		array[j][i] = '\0';
+		j++;
+		token = strtok(NULL, "  \t\n");
+	}
+	array[j] = NULL;
+	return (array);
 }
+
+/**
+ * number_of_words - return the number of sub strings
+ * in a string
+ * @str: string which contains sub strings
+ *
+ * Return: number of sub strings
+ */
 int number_of_words(char *str)
 {
-        int i, j = 0;
+	int i, j = 0;
 
-        if (str[0] == ' ' || str[0] == '\t' || str[0] == '\n')
-                i = 0;
-        if ((str[0] != ' ' && str[0] != '\t' && str[0] != '\n') && str[0] != '\0')
-                i = 1;
-        while (str[j] != '\0')
-        {
-                if ((str[j] == ' ' || str[j] == '\t' || str[j] == '\n')
-                                && (str[j + 1] != ' ' && str[j + 1] != '\t' && str[j + 1] != '\n')
-                                && str[j + 1] != '\0')
-                        i++;
-                j++;
-        }
-        return (i);
+	if (str[0] == ' ' || str[0] == '\t' || str[0] == '\n')
+		i = 0;
+	if ((str[0] != ' ' && str[0] != '\t' && str[0] != '\n') && str[0] != '\0')
+		i = 1;
+	while (str[j] != '\0')
+	{
+		if ((str[j] == ' ' || str[j] == '\t' || str[j] == '\n')
+		    && (str[j + 1] != ' ' && str[j + 1] != '\t' && str[j + 1] != '\n')
+		    && str[j + 1] != '\0')
+			i++;
+		j++;
+	}
+	return (i);
 }
