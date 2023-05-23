11ab8ea2dd9ea6f2559e3153d112bf34c0b9a0c9
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-23 22:47:57 +0300

    editing main file

--- prompt.c
+++ prompt.c
@@ -19,7 +19,8 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 		prompt();
 		if (getline(&string, &n, stdin) == -1)
 		{
-			write(STDOUT_FILENO, "\n", 1);
+			if (isatty(STDIN_FILENO) == 1)
+				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
 		if (string == NULL)
@@ -94,37 +95,4 @@ void free_maloc(char **array)
 		j--;
 	}
 	free(array);
-}
-/**
- * bultinenv - get variable enviriomment
- * @env: variable global envirionment system
- * Return: char value envirionment
- **/
-char *_getenv(char *name, char **env)
-{
-        char *token1 = NULL, *token2 = NULL;
-        char *env_name = NULL, *current_env = NULL;
-        int i = 0;
-
-        env_name = _strdup(name);
-
-        while (env[i] && env)
-        {
-                current_env = NULL;
-                token2 = NULL;
-                current_env = _strdup(env[i]);
-                token1 = strtok(current_env, "=");
-                token2 = _strdup(strtok(NULL, "="));
-                if (_strcmp(env_name, token1) == 0)
-                {
-                        break;
-                }
-                free(current_env);
-                free(token2);
-                i++;
-        }
-        free(current_env);
-        current_env = NULL;
-        free(env_name);
-        return (token2);
 }
