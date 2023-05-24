f14ddd17e12888b5203195e2ef50f990baeb62c2
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2023-05-24 19:54:01 +0300

    changing the prompt

--- prompt.c
+++ prompt.c
@@ -94,7 +94,7 @@ void _exec(char **arg, char **argv, char **env)
 	free(arg[0]);
 	arg[0]  = get_folder(cmd, env);
 	free(cmd);
-	if (arg[0] != NULL)
+	if (cmd != NULL)
 	{
 		pid = fork();
 		if (pid == 0)
